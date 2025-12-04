37dddf126f08ff994b678633bbe39e3003f38c8c
Konstanty Sośnierz <k@sosnierz.me>
2025-12-04 11:44:48 +0100

    Day 4

---
+++ day4/part1.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+// https://adventofcode.com/2025/day/4
+
+const char *fileName = "input";
+
+void*
+err_malloc(size_t size, char *err)
+{
+	void *ret = malloc(size);
+	if (ret == NULL) {
+		perror(err);
+		exit(1);
+	}
+	return ret;
+}
+
+struct Line {
+	char s[256]; // max 256 chars per line (the actual input only has 135, the example has 10)
+	uint8_t neighbours[256]; 
+	struct Line *next;
+};
+
+// returns linked list 
+struct Line*
+parse(const char *fileName)
+{
+	FILE *file = fopen(fileName, "r");
+	if (file == NULL) {
+		perror("fopen failed");
+		exit(1);
+	}
+
+	struct Line *ptr = err_malloc(sizeof(struct Line), "malloc in parse init fail");
+	struct Line *head = ptr;
+
+	while (fgets(ptr->s, sizeof(ptr->s), file)) {
+		ptr->s[strlen(ptr->s)-1] = 0; // drop newline
+		ptr->next = err_malloc(sizeof(struct Line), "struct malloc in parse loop fail");
+		ptr = ptr->next;
+	}
+
+	return head;
+}
+
+void
+printLine(struct Line* line)
+{
+	// debug
+	for (int i = 0; i < strlen(line->s); i++) {
+		if (line->s[i] == '.') {
+			printf(".");
+		} else {
+			if (line->neighbours[i] < 4)
+				printf("x");
+			else
+				printf("@");
+		}
+	}
+	printf("\n");
+}
+
+uint8_t
+analyzeLine(struct Line* line)
+{
+	// line: increment neighbours[i] for each '@' neighbouring s[i] in the X axis
+	
+	for (int i = 0; i < strlen(line->s); i++) {
+		if (i > 0 && line->s[i-1] == '@')
+			line->neighbours[i]++;
+		if (i+1 < strlen(line->s) && line->s[i+1] == '@')
+			line->neighbours[i]++;
+	}
+		
+	// line, line2: increment neighbours[i] for each '@' located at s[i-1], s[i], s[i+1]
+	struct Line* line2 = line->next;
+
+	if (line2 != NULL) {
+		for (int i = 0; i < strlen(line->s); i++) {
+			if (line->s[i] == '@') {
+				if (i > 0)
+					line2->neighbours[i-1]++;
+				line2->neighbours[i]++;
+				if (i+1 < strlen(line->s))
+					line2->neighbours[i+1]++;
+			}
+			if (line2->s[i] == '@') {
+				if (i > 0)
+					line->neighbours[i-1]++;
+				line->neighbours[i]++;
+				if (i+1 < strlen(line->s))
+					line->neighbours[i+1]++;
+			}
+		}
+	}
+
+	// return the number of rolls of paper that neighbour <4 other rolls of paper in line
+	uint8_t count = 0;
+	for (int i = 0; i < strlen(line->s); i++) {
+		if (line->s[i] == '@')
+			if(line->neighbours[i]<4)
+				count++;
+	}
+
+	//printLine(line);
+
+	return count;
+}
+
+int
+main()
+{
+	uint64_t sum = 0;
+	struct Line *input = parse(fileName);
+
+	for (struct Line *ptr = input; ptr != NULL; ptr = ptr->next)
+		sum += analyzeLine(ptr);
+
+	printf("The sum equals %ld\n", sum);
+	return 0;
+}
